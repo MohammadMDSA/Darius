01849d4bab0916b1cf66888bb892ceb7f6bdc329
Mohammad MDSA <samimd.77@gmail.com>
2022-06-14 16:47:58 +0430

    Add asserts using boost

--- src/Utils/Assert.hpp
+++ src/Utils/Assert.hpp
@@ -1,11 +1,15 @@
 #include <cassert>
 #include <stdexcept>
+#include <boost/assert.hpp>
+#include <boost/static_assert.hpp>
 
-#ifdef _DEBUG
-#define D_Assert(cond)		assert(cond); _assume(cond)
-#else
-#define D_Assert(cond)		_assume(cond)
-#endif
+#define D_ASSERT(cond)			BOOST_ASSERT(cond)
+#define D_ASSERT_M(cond, msg)	BOOST_ASSERT_MSG(cond, msg)
+#define D_VERIFY(cond)			BOOST_VERIFY(cond)
+#define D_VERIFY_M(cond, msg)	BOOST_VERIFY_MSG(cond, msg)
+#define D_STATIC_ASSERT(cond)	BOOST_STATIC_ASSERT(cond)
+#define D_STATIC_ASSERT_MSG(cond, msg) BOOST_STATIC_ASSERT_MSG(cond, msg)
+
 
 #define D_Error(msg, ...)	throw std::runtime_error(msg)
 
