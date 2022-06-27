82ef494aa2376fc9da54fd83fdae5cc8da60dede
Mohammad MDSA <samimd.77@gmail.com>
2022-06-27 11:33:06 +0430

    Add buffer helper (Upload buffer wrapper)

---
+++ src/Renderer/Utils/D3DUtils.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Darius::Renderer::Utils
+{
+	Microsoft::WRL::ComPtr<ID3D12Resource> CreateDefaultBuffer(
+		ID3D12Device* device,
+		ID3D12GraphicsCommandList* cmdList,
+		const void* initData,
+		UINT64 byteSize,
+		Microsoft::WRL::ComPtr<ID3D12Resource>& uploadBuffer
+	);
+}
