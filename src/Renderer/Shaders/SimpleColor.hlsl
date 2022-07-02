69a65a9b3e9b0264e43aa588983eb99eef46cb4c
Mohammad MDSA <samimd.77@gmail.com>
2022-07-02 11:24:01 +0430

    Add a simple color shader

---
+++ src/Renderer/Shaders/SimpleColor.hlsl
@@ -0,0 +1,34 @@
+cbuffer cbPerObject : register(b0);
+{
+	float4x4 gWorldViewProj;
+};
+
+struct VertexIn
+{
+	float3 pos : POSITIONT;
+	float4 color : COLOR;
+};
+
+struct PixelIn
+{
+	float4 pos : SV_POSITION;
+	float4 color : COLOR;
+};
+
+PixelIn VS(VertexIn vin)
+{
+	PixelIn vout;
+	
+	// Transform homogeneous to clip space.
+	vout.pos = mul(float4(vin.pos, 1.0f), gWorldViewProj);
+	
+	// Passing color to ps
+	vout.color = vin.color;
+	
+	return vout;
+}
+
+float4 PS(PixelIn pin) : SV_TARGET
+{
+	return pin.color;
+}
