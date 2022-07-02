1143647ce339dd2f26257c8ef726bcbbe2d952e0
Mohammad MDSA <samimd.77@gmail.com>
2022-07-02 11:24:46 +0430

    Add vertex/index buffer upload helpers

--- src/Renderer/GraphicsUtils/D3DUtils.cpp
+++ src/Renderer/GraphicsUtils/D3DUtils.cpp
@@ -3,7 +3,7 @@
 
 using namespace Microsoft::WRL;
 
-namespace Darius::Renderer::Utils
+namespace Darius::Renderer::GraphicsUtils
 {
 	Microsoft::WRL::ComPtr<ID3D12Resource> CreateDefaultBuffer(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList, const void* initData, UINT64 byteSize, Microsoft::WRL::ComPtr<ID3D12Resource>& uploadBuffer)
 	{
---
+++ src/Renderer/GraphicsUtils/D3DUtils.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+namespace Darius::Renderer::GraphicsUtils
+{
+	Microsoft::WRL::ComPtr<ID3D12Resource> CreateDefaultBuffer(
+		ID3D12Device* device,
+		ID3D12GraphicsCommandList* cmdList,
+		const void* initData,
+		UINT64 byteSize,
+		Microsoft::WRL::ComPtr<ID3D12Resource>& uploadBuffer
+	);
+
+    inline UINT CalcConstantBufferByteSize(UINT byteSize)
+    {
+        // Constant buffers must be a multiple of the minimum hardware
+        // allocation size (usually 256 bytes).  So round up to nearest
+        // multiple of 256.  We do this by adding 255 and then masking off
+        // the lower 2 bytes which store all bits < 256.
+        // Example: Suppose byteSize = 300.
+        // (300 + 255) & ~255
+        // 555 & ~255
+        // 0x022B & ~0x00ff
+        // 0x022B & 0xff00
+        // 0x0200
+        // 512
+        return (byteSize + 255) & ~255;
+    }
+}
