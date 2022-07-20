6b868439bf3911e3744438c439d6822851505df1
Mohammad MDSA <samimd.77@gmail.com>
2022-07-20 13:05:18 +0430

    Add PositionNormalTangentTexture vertex type

--- src/Renderer/GraphicsUtils/VertexTypes.cpp
+++ src/Renderer/GraphicsUtils/VertexTypes.cpp
@@ -14,7 +14,7 @@ namespace Darius::Renderer::GraphicsUtils::VertexTypes
 		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
 	};
 
-	D_STATIC_ASSERT_M(sizeof(VertexPosition) == 16, "Vertex struct/layout mismatch");
+	D_STATIC_ASSERT_M(sizeof(VertexPosition) == 12, "Vertex struct/layout mismatch");
 
 	const D3D12_INPUT_LAYOUT_DESC VertexPosition::InputLayout =
 	{
@@ -31,11 +31,31 @@ namespace Darius::Renderer::GraphicsUtils::VertexTypes
 		{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
 	};
 
-	D_STATIC_ASSERT_M(sizeof(VertexPositionColor) == 32, "Vertex struct/layout mismatch");
+	D_STATIC_ASSERT_M(sizeof(VertexPositionColor) == 28, "Vertex struct/layout mismatch");
 
 	const D3D12_INPUT_LAYOUT_DESC VertexPositionColor::InputLayout =
 	{
 		VertexPositionColor::InputElements,
 		VertexPositionColor::InputElementCount
 	};
+
+	/////////////////////////////////////////////////
+	////////// Position Normal Tangent UV ///////////
+	/////////////////////////////////////////////////
+	const D3D12_INPUT_ELEMENT_DESC VertexPositionNormalTangentTexture::InputElements[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+		{ "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+		{ "TANGENT", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
+	};
+
+	D_STATIC_ASSERT_M(sizeof(VertexPositionNormalTangentTexture) == 44, "Vertex struct/layout mismatch");
+
+	const D3D12_INPUT_LAYOUT_DESC VertexPositionNormalTangentTexture::InputLayout =
+	{
+		VertexPositionNormalTangentTexture::InputElements,
+		VertexPositionNormalTangentTexture::InputElementCount
+	};
+
 }
--- src/Renderer/GraphicsUtils/VertexTypes.hpp
+++ src/Renderer/GraphicsUtils/VertexTypes.hpp
@@ -2,6 +2,8 @@
 
 #include <Math/Vector.hpp>
 
+#define D_RENDERER_VERTEX Darius::Renderer::GraphicsUtils::VertexTypes
+
 using namespace Darius::Math;
 
 namespace Darius::Renderer::GraphicsUtils::VertexTypes
@@ -28,7 +30,7 @@ namespace Darius::Renderer::GraphicsUtils::VertexTypes
 		{
 		}
 
-		Vector3									mPosition;
+		XMFLOAT3								mPosition;
 
 		static const D3D12_INPUT_LAYOUT_DESC	InputLayout;
 
@@ -59,15 +61,55 @@ namespace Darius::Renderer::GraphicsUtils::VertexTypes
 		{
 		}
 
-
-
-		Vector3									mPosition;
-		Vector4									mColor;
+		XMFLOAT3								mPosition;
+		XMFLOAT4								mColor;
 
 		static const D3D12_INPUT_LAYOUT_DESC	InputLayout;
 
 	private:
 		static constexpr unsigned int			InputElementCount = 2;
+		static const D3D12_INPUT_ELEMENT_DESC	InputElements[InputElementCount];
+	};
+
+	struct VertexPositionNormalTangentTexture : public IVertexType
+	{
+		VertexPositionNormalTangentTexture() = default;
+
+		VertexPositionNormalTangentTexture(const VertexPositionNormalTangentTexture&) = default;
+		VertexPositionNormalTangentTexture& operator=(const VertexPositionNormalTangentTexture&) = default;
+
+		VertexPositionNormalTangentTexture(VertexPositionNormalTangentTexture&&) = default;
+		VertexPositionNormalTangentTexture& operator=(VertexPositionNormalTangentTexture&&) = default;
+
+		VertexPositionNormalTangentTexture(Vector3 const& pos, Vector3 const& norm, Vector3 const& tang, XMFLOAT2 const& uv) :
+			mPosition(pos),
+			mNormal(norm),
+			mTangent(tang),
+			mTexC(uv)
+		{
+		}
+
+		VertexPositionNormalTangentTexture(
+			float px, float py, float pz,
+			float nx, float ny, float nz,
+			float tx, float ty, float tz,
+			float u, float v) :
+			mPosition(px, py, pz),
+			mNormal(nx, ny, nz),
+			mTangent(tx, ty, tz),
+			mTexC(u, v)
+		{
+		}
+
+		XMFLOAT3								mPosition;
+		XMFLOAT3								mNormal;
+		XMFLOAT3								mTangent;
+		XMFLOAT2								mTexC;
+
+		static const D3D12_INPUT_LAYOUT_DESC	InputLayout;
+
+	private:
+		static constexpr unsigned int			InputElementCount = 4;
 		static const D3D12_INPUT_ELEMENT_DESC	InputElements[InputElementCount];
 	};
 }
