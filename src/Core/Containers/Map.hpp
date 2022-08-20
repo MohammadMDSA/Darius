e0a9719bfec69c997b62cd5095046c94ad2c95f9
Mohammad MDSA <samimd.77@gmail.com>
2022-08-20 17:52:41 +0430

    Add hash class to DMap template

--- src/Core/Containers/Map.hpp
+++ src/Core/Containers/Map.hpp
@@ -10,7 +10,7 @@
 namespace Darius::Core::Containers
 {
 	// TODO: Use custom allocators using preallocated memory
-	template<class K, class V, class Alloc = std::allocator<std::pair<const K, V>>>
-	using DMap = std::unordered_map<K, V, std::hash<K>, std::equal_to<K>, Alloc>;
+	template<class K, class V, class Hash = std::hash<K>, class Alloc = std::allocator<std::pair<const K, V>>>
+	using DMap = std::unordered_map<K, V, Hash, std::equal_to<K>, Alloc>;
 
 }
