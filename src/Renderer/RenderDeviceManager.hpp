7537717aae59a9d5bbf56197f20d6092cb97223b
Mohammad MDSA <samimd.77@gmail.com>
2022-08-23 12:51:26 +0430

    Minor changes

--- src/Renderer/RenderDeviceManager.hpp
+++ src/Renderer/RenderDeviceManager.hpp
@@ -12,11 +12,11 @@ namespace Darius::Renderer::Device
 	void Initialize(HWND window, int width, int height);
 	void Shutdown();
 
-	FrameResource* GetCurrentFrameResource();
-	ID3D12Device* GetDevice();
-	FrameResource* GetFrameResourceWithIndex(int i);
-	DXGI_FORMAT GetBackBufferFormat();
-	DXGI_FORMAT GetDepthBufferFormat();
+	FrameResource*	GetCurrentFrameResource();
+	ID3D12Device*	GetDevice();
+	FrameResource*	GetFrameResourceWithIndex(int i);
+	DXGI_FORMAT		GetBackBufferFormat();
+	DXGI_FORMAT		GetDepthBufferFormat();
 
 	// Window functions
 	void OnWindowMoved();
