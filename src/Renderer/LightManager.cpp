e0505bb377df1f8a23218b0385a678f421ee3c6e
Mohammad MDSA <samimd.77@gmail.com>
2022-08-29 13:52:43 +0430

    Add light activation bit mask and containers to light manager

--- src/Renderer/LightManager.cpp
+++ src/Renderer/LightManager.cpp
@@ -1,9 +1,119 @@
 #include "pch.hpp"
 #include "LightManager.hpp"
 
+#include "FrameResource.hpp"
+#include "GraphicsUtils/Buffers/GpuBuffer.hpp"
+#include "GraphicsUtils/Buffers/UploadBuffer.hpp"
+#include "RenderDeviceManager.hpp"
+
+using namespace D_GRAPHICS_BUFFERS;
 
 namespace Darius::Renderer::LightManager
 {
+	bool				_initialized = false;
 
+	DVector<LightData>	DirectionalLights;
+	DVector<LightData>	PointLights;
+	DVector<LightData>	SpotLights;
+
+	DVector<bool>		DirLightActive;
+	DVector<bool>		PointLightActive;
+	DVector<bool>		SpotLightActive;
+
+	D_GRAPHICS_BUFFERS::UploadBuffer ActiveLightsUpload[D_RENDERER_FRAME_RESOUCE::gNumFrameResources];
+	ByteAddressBuffer	ActiveLightsBufferGpu;
+
+	void Initialize()
+	{
+		D_ASSERT(!_initialized);
+		DirectionalLights.resize(MaxNumDirectionalLight);
+		DirLightActive.resize(MaxNumDirectionalLight);
+		PointLights.resize(MaxNumPointLight);
+		PointLightActive.resize(MaxNumPointLight);
+		SpotLights.resize(MaxNumSpotLight);
+		SpotLightActive.resize(MaxNumSpotLight);
+
+		for (size_t i = 0; i < MaxNumDirectionalLight; i++)
+		{
+			DirLightActive[i] = false;
+		}
+
+		for (size_t i = 0; i < MaxNumPointLight; i++)
+		{
+			PointLightActive[i] = false;
+		}
+
+		for (size_t i = 0; i < MaxNumSpotLight; i++)
+		{
+			SpotLightActive[i] = false;
+		}
+		PointLightActive[0] = true;
+
+		// Initializing buffers
+		size_t elemSize = sizeof(UINT) * 8;
+		size_t count = (MaxNumLight + elemSize - 1) / elemSize;
+		for (size_t i = 0; i < D_RENDERER_FRAME_RESOUCE::gNumFrameResources; i++)
+		{
+			ActiveLightsUpload[i].Create(L"Active Light Upload", count * sizeof(UINT));
+		}
+		ActiveLightsBufferGpu.Create(L"Active Light Buffer", count, sizeof(UINT), nullptr);
+
+	}
+
+	void Shutdown()
+	{
+		D_ASSERT(_initialized);
+	}
+
+	void UpdateBuffers(D_GRAPHICS::GraphicsContext& context)
+	{
+		auto& currentUpload = ActiveLightsUpload[D_RENDERER_DEVICE::GetCurrentResourceIndex()];
+		UINT* data = (UINT*)currentUpload.Map();
+
+		bool done = false;
+
+		for (size_t i = 0; !done && i < (MaxNumLight + sizeof(UINT) * 8 - 1) / sizeof(UINT) * 8; i++)
+		{
+			UINT activeFlags = 0;
+
+			for (size_t bitIdx = 0; bitIdx < sizeof(UINT) * 8; bitIdx++)
+			{
+				auto lightIdx = i * bitIdx * sizeof(UINT) + bitIdx;
+
+				// Setting light status
+				bool lightStatus = false;
+				if (lightIdx < MaxNumDirectionalLight)
+					lightStatus = DirLightActive[lightIdx];
+				else if (lightIdx < MaxNumDirectionalLight + MaxNumPointLight)
+					lightStatus = PointLightActive[lightIdx - MaxNumDirectionalLight];
+				else if (lightIdx < MaxNumDirectionalLight + MaxNumPointLight + MaxNumSpotLight)
+					lightStatus = SpotLightActive[lightIdx - MaxNumDirectionalLight - MaxNumPointLight];
+				else
+				{
+					done = true;
+					break;
+				}
+
+				if (lightStatus)
+					activeFlags =+ 1;
+				activeFlags <<= 1;
+			}
+			auto tt = sizeof(UINT);
+			data[i] = activeFlags;
+		}
+
+		currentUpload.Unmap();
+
+		// Uploading...
+		context.TransitionResource(ActiveLightsBufferGpu, D3D12_RESOURCE_STATE_COPY_DEST, true);
+		context.GetCommandList()->CopyBufferRegion(ActiveLightsBufferGpu.GetResource(), 0, currentUpload.GetResource(), 0, currentUpload.GetBufferSize());
+		context.TransitionResource(ActiveLightsBufferGpu, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
+
+	}
+
+	D3D12_CPU_DESCRIPTOR_HANDLE GetLightMaskHandle()
+	{
+		return ActiveLightsBufferGpu.GetSRV();
+	}
 }
 
--- src/Renderer/LightManager.hpp
+++ src/Renderer/LightManager.hpp
@@ -1,19 +1,27 @@
 #pragma once
 
+#include "CommandContext.hpp"
+
+#include <Core/Containers/Vector.hpp>
 #include <Math/VectorMath.hpp>
 
 #ifndef D_LIGHT
 #define D_LIGHT Darius::Renderer::LightManager
 #endif // !D_LIGHT
 
+using namespace D_CONTAINERS;
 using namespace D_MATH;
 
 namespace Darius::Renderer::LightManager
 {
+	constexpr int		MaxNumDirectionalLight = 6;
+	constexpr int		MaxNumPointLight = 125;
+	constexpr int		MaxNumSpotLight = 125;
+	constexpr int		MaxNumLight = MaxNumDirectionalLight + MaxNumPointLight + MaxNumSpotLight;
 
 	struct LightData
 	{
-		XMFLOAT4		Color = Vector4(kZero);
+		XMFLOAT4		Color = Vector4(kOne);
 		XMFLOAT4		Direction = {0.f, 0.f, -1.f, 0.f };// Directional/Spot light only
 		XMFLOAT4		Position = Vector4(kZero);  // Point light only
 		float			FalloffStart = 1.f;			// Point/Spot light only
@@ -22,4 +30,9 @@ namespace Darius::Renderer::LightManager
 
 	};
 
+	void				Initialize();
+	void				Shutdown();
+
+	void				UpdateBuffers(D_GRAPHICS::GraphicsContext& context);
+	D3D12_CPU_DESCRIPTOR_HANDLE GetLightMaskHandle();
 }
