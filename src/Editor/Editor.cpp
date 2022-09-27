7d8d8c4a288cab126936de41443f7221ef722c09
Mohammad MDSA <samimd.77@gmail.com>
2022-09-27 12:00:44 +0330

    Minor changes

--- src/Editor/Editor.cpp
+++ src/Editor/Editor.cpp
@@ -115,7 +115,7 @@ namespace Darius::Editor
 	// Updates the world.
 	void Editor::Update(D_TIME::StepTimer const& timer)
 	{
-		D_PROFILING::ScopedTimer _profiler(L"Update");
+		D_PROFILING::ScopedTimer _profiler(L"Update CPU");
 		PIXBeginEvent(PIX_COLOR_DEFAULT, L"Update");
 
 		float elapsedTime = float(timer.GetElapsedSeconds());
