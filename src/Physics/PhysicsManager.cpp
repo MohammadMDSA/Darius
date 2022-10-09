a664d4a6ea9ad0fc7da992ec2a0504ce60faf676
Mohammad MDSA <samimd.77@gmail.com>
2022-10-10 02:17:30 +0330

    Add physics manager (init, shutdown, update, getters, ...)

--- src/Physics/PhysicsManager.cpp
+++ src/Physics/PhysicsManager.cpp
@@ -1,38 +1,158 @@
 #include "pch.hpp"
 #include "PhysicsManager.hpp"
+#include "Physics/Components/ColliderComponent.hpp"
 
+#include <Core/TimeManager/TimeManager.hpp>
+#include <Scene/Scene.hpp>
+#include <Renderer/GraphicsUtils/Profiling/Profiling.hpp>
+#include <Job/Job.hpp>
 #include <Utils/Assert.hpp>
 
 #include <PxPhysics.h>
-#include <PxPhysicsAPI.h>
+
+#define PX_RELEASE(x)	if(x)	{ x->release(); x = NULL;	}
 
 using namespace physx;
 
 namespace Darius::Physics
 {
-	bool										_init = false;
+	bool									_init = false;
 
-	physx::PxDefaultAllocator		gAllocator;
-	physx::PxDefaultErrorCallback	gErrorCallback;
+	PxDefaultAllocator						gAllocator;
+	PxDefaultErrorCallback					gErrorCallback;
+	PxTolerancesScale						gToleranceScale;
 
-	physx::PxFoundation* gFoundation = NULL;
-	physx::PxPhysics* gPhysics = NULL;
+	PxFoundation* gFoundation = NULL;
+	PxPhysics* gPhysics = NULL;
 
-	physx::PxDefaultCpuDispatcher* gDispatcher = NULL;
-	physx::PxScene* gScene = NULL;
+	PxDefaultCpuDispatcher* gDispatcher = NULL;
+	PxScene* gScene = NULL;
 
-	physx::PxMaterial* gMaterial = NULL;
+	PxMaterial* gDefaultMaterial = NULL;
 
-	physx::PxPvd* gPvd = NULL;
+	PxPvd* gPvd = NULL;
+
+	void					UpdatePostPhysicsTransforms();
+	void					UpdatePrePhysicsTransform();
 
 	void Initialize()
 	{
 		D_ASSERT(!_init);
 		_init = true;
+
+		gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gAllocator, gErrorCallback);
+
+#ifdef _DEBUG
+		gPvd = PxCreatePvd(*gFoundation);
+		PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
+		gPvd->connect(*transport, PxPvdInstrumentationFlag::eALL);
+#endif
+
+		gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, gToleranceScale, true, gPvd);
+
+		PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
+		sceneDesc.gravity = PxVec3(0.f, -9.8f, 0.f);
+		gDispatcher = PxDefaultCpuDispatcherCreate(2);
+		sceneDesc.cpuDispatcher = gDispatcher;
+		sceneDesc.filterShader = PxDefaultSimulationFilterShader;
+		gScene = gPhysics->createScene(sceneDesc);
+
+#ifdef _DEBUG
+		PxPvdSceneClient* pvdClient = gScene->getScenePvdClient();
+		if (pvdClient)
+		{
+			pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+			pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+			pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+		}
+#endif // _DEBUG
+
+		gDefaultMaterial = gPhysics->createMaterial(0.5f, 0.5f, 0.6f);
+
+		ColliderComponent::StaticConstructor();
 	}
 
 	void Shutdown()
 	{
 		D_ASSERT(_init);
+
+		PX_RELEASE(gScene);
+		PX_RELEASE(gDispatcher);
+		PX_RELEASE(gPhysics);
+
+		if (gPvd)
+		{
+			auto transport = gPvd->getTransport();
+			gPvd->release();
+			gPvd = nullptr;
+			PX_RELEASE(transport);
+		}
+
+		PX_RELEASE(gFoundation);
 	}
+
+	void Update()
+	{
+		D_PROFILING::ScopedTimer physicsProfiler(L"Physics Update");
+
+		UpdatePrePhysicsTransform();
+
+		{
+			D_PROFILING::ScopedTimer physicsProfiler(L"Physics Simulation Update");
+			gScene->simulate(D_TIME::GetTargetElapsedSeconds());
+			gScene->fetchResults(true);
+		}
+
+		UpdatePostPhysicsTransforms();
+	}
+
+	void UpdatePostPhysicsTransforms()
+	{
+		D_PROFILING::ScopedTimer physicsProfiler(L"Physics Post Update");
+		D_WORLD::GetRegistry().each([&](ColliderComponent& colliderComp)
+			{
+				D_JOB::AssignTask([&](int threadNumber, int)
+					{
+						colliderComp.Update(-1);
+
+					});
+			}
+		);
+
+		if (D_JOB::IsMainThread())
+			D_JOB::WaitForThreadsToFinish();
+	}
+
+	void UpdatePrePhysicsTransform()
+	{
+		D_PROFILING::ScopedTimer physicsProfiler(L"Physics Post Update");
+		D_WORLD::GetRegistry().each([&](ColliderComponent& colliderComp)
+			{
+				D_JOB::AssignTask([&](int threadNumber, int)
+					{
+						colliderComp.PreUpdate();
+
+					});
+			}
+		);
+
+		if (D_JOB::IsMainThread())
+			D_JOB::WaitForThreadsToFinish();
+	}
+
+	PxScene* GetScene()
+	{
+		return gScene;
+	}
+
+	PxPhysics* GetCore()
+	{
+		return gPhysics;
+	}
+
+	PxMaterial const* GetDefaultMaterial()
+	{
+		return gDefaultMaterial;
+	}
+
 }
--- src/Physics/PhysicsManager.hpp
+++ src/Physics/PhysicsManager.hpp
@@ -1,11 +1,61 @@
 #pragma once
 
+#include <Math/VectorMath.hpp>
+
+#include <PxPhysicsAPI.h>
+
 #ifndef D_PHYSICS
 #define D_PHYSICS Darius::Physics
 #endif // !D_PHYSICS
 
 namespace Darius::Physics
 {
-	void Initialize();
-	void Shutdown();
+	void					Initialize();
+	void					Shutdown();
+
+	void					Update();
+
+	physx::PxScene*			GetScene();
+	physx::PxPhysics*		GetCore();
+	physx::PxMaterial const* GetDefaultMaterial();
+
+	INLINE physx::PxQuat GetQuat(D_MATH::Quaternion const& quat)
+	{
+		return physx::PxQuat(quat.GetX(), quat.GetY(), quat.GetZ(), quat.GetW());
+	}
+
+	INLINE D_MATH::Quaternion GetQuat(physx::PxQuat const& quat)
+	{
+		return D_MATH::Quaternion(quat.x, quat.y, quat.z, quat.w);
+	}
+	
+	INLINE physx::PxVec3 GetVec3(D_MATH::Vector3 const& vec3)
+	{
+		return physx::PxVec3(vec3.GetX(), vec3.GetY(), vec3.GetZ());
+	}
+
+	INLINE D_MATH::Vector3 GetVec3(physx::PxVec3 const& vec3)
+	{
+		return D_MATH::Vector3(vec3.x, vec3.y, vec3.z);
+	}
+
+	INLINE physx::PxVec4 GetVec4(D_MATH::Vector4 const& vec4)
+	{
+		return physx::PxVec4(vec4.GetX(), vec4.GetY(), vec4.GetZ(), vec4.GetW());
+	}
+
+	INLINE D_MATH::Vector4 GetVec4(physx::PxVec4 const& vec4)
+	{
+		return D_MATH::Vector4(vec4.x, vec4.y, vec4.z, vec4.w);
+	}
+
+	INLINE physx::PxTransform GetTransform(D_MATH::Transform const& trans)
+	{
+		return physx::PxTransform(GetVec3(trans.Translation), GetQuat(trans.Rotation));
+	}
+
+	INLINE D_MATH::Transform GetTransform(physx::PxTransform const& trans)
+	{
+		return D_MATH::Transform(GetVec3(trans.p), GetQuat(trans.q));
+	}
 }
