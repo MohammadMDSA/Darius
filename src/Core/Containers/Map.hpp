26e798d6cc29ea970c3aeae81a5b2a9951696253
Mohammad MDSA <samimd.77@gmail.com>
2022-10-22 17:15:57 +0330

    Add ordered map ds

--- src/Core/Containers/Map.hpp
+++ src/Core/Containers/Map.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <unordered_map>
+#include <map>
 #include <memory>
 
 #ifndef D_CONTAINERS
@@ -10,7 +11,10 @@
 namespace Darius::Core::Containers
 {
 	// TODO: Use custom allocators using preallocated memory
-	template<class K, class V, class Hash = std::hash<K>, class Alloc = std::allocator<std::pair<const K, V>>>
-	using DMap = std::unordered_map<K, V, Hash, std::equal_to<K>, Alloc>;
+	template<typename K, typename V, typename Hash = std::hash<K>, typename Alloc = std::allocator<std::pair<const K, V>>>
+	using DUnorderedMap = std::unordered_map<K, V, Hash, std::equal_to<K>, Alloc>;
 
+	// TODO: Use custom allocators using preallocated memory
+	template<typename K, typename V, typename Comp = std::less<K>, typename Alloc = std::allocator<std::pair<const K, V>>>
+	using DMap = std::map<K, V, Comp, Alloc>;
 }
