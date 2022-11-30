938628403cee398a1cdf75833810d022e0cb05ae
Mohammad MDSA <samimd.77@gmail.com>
2022-11-30 21:44:14 +0330

    Add hash and equal types access for DSet

--- src/Core/Containers/Set.hpp
+++ src/Core/Containers/Set.hpp
@@ -10,7 +10,7 @@
 namespace Darius::Core::Containers
 {
 	// Use custom allocators using preallocated memory
-	template<typename T, typename Alloc = std::allocator<T>>
-	using DSet = std::unordered_set<T, std::hash<T>, std::equal_to<T>, Alloc>;
+	template<typename T, typename Hash = std::hash<T>, typename Equal = std::equal_to<T>, typename Alloc = std::allocator<T>>
+	using DSet = std::unordered_set<T, Hash, Equal, Alloc>;
 
 }
