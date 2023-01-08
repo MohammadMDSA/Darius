499d9a0ee49d791239c0eda94f2d8b7c0aa6378e
Mohammad MDSA <samimd.77@gmail.com>
2023-01-09 01:03:45 +0330

    Add simple content window items

--- src/Editor/GUI/Components/ContentWindowComponents.cpp
+++ src/Editor/GUI/Components/ContentWindowComponents.cpp
@@ -1,24 +1,46 @@
 #include "Editor/pch.hpp"
 
 #include "ContentWindowComponents.hpp"
+#include "Editor/GUI/GuiManager.hpp"
 
 #include <imgui.h>
+#include <Libs/FontIcon/IconsFontAwesome6.h>
 
 namespace Darius::Editor::Gui::Component
 {
 
 	void ContentWindowItemGrid(EditorContentWindowItem& data, float width, float height, bool& selected, bool& doubleClicked)
 	{
+
 		auto nameStr = data.Name.c_str();
 		ImGui::PushID(nameStr);
 
-		if(data.IsDirectory)
-		ImGui::PushStyleColor(ImGuiCol_FrameBg, ImVec4(0.6f, 0.1f, 0.1f, 1.f));
+		ImGui::BeginGroup();
 
-		doubleClicked = ImGui::Selectable(nameStr, &selected, ImGuiSelectableFlags_AllowDoubleClick, ImVec2(width, height));
+		auto padding = 15.f;
+		auto paddingVec = ImVec2(padding, padding);
+		auto size = ImVec2(width - 2 * padding, height - 2 * padding);
+
+		auto startCurPos = ImGui::GetCursorPos();
+
+		ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, paddingVec);
 
 		if (data.IsDirectory)
-			ImGui::PopStyleColor();
+			ImGui::ImageButton("##icon", (ImTextureID)D_GUI_MANAGER::GetIconTextureId(D_GUI_MANAGER::Icon::Folder), size);
+
+		else
+			ImGui::ImageButton("##icon", (ImTextureID)D_GUI_MANAGER::GetIconTextureId(D_GUI_MANAGER::Icon::File), size);
+
+		ImGui::PopStyleVar();
+
+		ImGui::SetCursorPos(ImVec2(startCurPos.x, startCurPos.y + height - padding + 2));
+		
+		//ImGui::TextWrapped(data.Name.c_str(), );
+
+		selected = false;
+		doubleClicked = ImGui::IsMouseDoubleClicked(0) && ImGui::IsItemHovered();
+
+		ImGui::EndGroup();
 
 		ImGui::PopID();
 	}
