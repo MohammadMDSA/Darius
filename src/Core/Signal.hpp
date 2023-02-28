67790de4760c4f21cb87f3d07d57a5d8f737f53e
Mohammad MDSA <samimd.77@gmail.com>
2023-02-28 22:49:46 +0330

    Add signal helpers

--- src/Core/Signal.hpp
+++ src/Core/Signal.hpp
@@ -2,6 +2,8 @@
 
 #include <boost/signals2.hpp>
 
+#include <functional>
+
 #ifndef D_CORE
 #define D_CORE Darius::Core
 #endif // !D_UTILS
@@ -11,5 +13,19 @@ namespace Darius::Core
 
 	template<typename T>
 	using Signal = boost::signals2::signal<T>;
+	using SignalConnection = boost::signals2::connection;
 
 }
+
+#define D_H_SIGNAL_DEFINITION(name, ...) \
+D_CORE::Signal<__VA_ARGS__> name##Signal; \
+SignalConnection SubscribeOn##name(std::function<__VA_ARGS__> callback) { return name##Signal.connect(callback); }
+
+#define D_H_SIGNAL_DECL(name, ...) \
+SignalConnection SubscribeOn##name(std::function<__VA_ARGS__> callback);
+
+#define D_CH_SIGNAL(name, ...) \
+private: \
+D_CORE::Signal<__VA_ARGS__> name##Signal; \
+public: \
+INLINE SignalConnection SubscribeOn##name(std::function<__VA_ARGS__> callback) { return name##Signal.connect(callback); }
