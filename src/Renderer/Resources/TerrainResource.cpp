d671dd2e2458ab2c83dd57db46f33fcad7d24a0b
Mohammad MDSA <samimd.77@gmail.com>
2023-06-04 12:59:41 +0330

    Add terrain resource

---
+++ src/Renderer/Resources/TerrainResource.cpp
@@ -0,0 +1,146 @@
+#include "Renderer/pch.hpp"
+#include "TerrainResource.hpp"
+
+#include "Renderer/Renderer.hpp"
+
+#include <ResourceManager/ResourceManager.hpp>
+#include <Utils/Common.hpp>
+
+#ifdef _D_EDITOR
+#include <ResourceManager/ResourceDragDropPayload.hpp>
+#include <Libs/FontIcon/IconsFontAwesome6.h>
+#include <imgui.h>
+#endif
+
+#include "TerrainResource.sgenerated.hpp"\
+
+using namespace D_SERIALIZATION;
+
+namespace Darius::Graphics
+{
+
+	D_CH_RESOURCE_DEF(TerrainResource);
+
+	struct TerrainParametersConstants
+	{
+		float HeightFactor;
+	};
+
+	void TerrainResource::WriteResourceToFile(D_SERIALIZATION::Json& j) const
+	{
+		Json json;
+
+		D_H_SERIALIZE(HeightFactor);
+
+		if (mHeightMap.IsValid())
+		{
+			json["HeightMap"] = D_CORE::ToString(mHeightMap->GetUuid());
+		}
+
+		D_FILE::WriteJsonFile(GetPath(), json);
+	}
+
+	void TerrainResource::ReadResourceFromFile(D_SERIALIZATION::Json const& j)
+	{
+		Json json;
+		D_FILE::ReadJsonFile(GetPath(), json);
+
+		D_H_DESERIALIZE(HeightFactor);
+
+		if (json.contains("HeightMap"))
+		{
+			mHeightMap = D_RESOURCE::GetResource<TextureResource>(D_CORE::FromString(json["HeightMap"]), GetAsCountedOwner());
+		}
+	}
+
+	bool TerrainResource::UploadToGpu()
+	{
+		// Creating buffers
+		if (mParametersConstantsGPU.GetGpuVirtualAddress() == D3D12_GPU_VIRTUAL_ADDRESS_NULL)
+		{
+			// Initializing patameters constants buffers
+			for (size_t i = 0; i < D_RENDERER_FRAME_RESOURCE::gNumFrameResources; i++)
+			{
+				mParametersConstantsCPU[i].Create(L"Terrain Parameter Constants Buffer: " + GetName(), sizeof(TerrainParametersConstants));
+			}
+			mParametersConstantsGPU.Create(L"Terrain Parameter Constants Buffer: " + GetName(), 1, sizeof(TerrainParametersConstants));
+
+			mTexturesHeap = D_RENDERER::AllocateTextureDescriptor(1);
+		}
+
+		if (!mHeightMap.IsValid())
+			mHeightMap = D_RESOURCE::GetResource<TextureResource>(D_GRAPHICS::GetDefaultGraphicsResource(D_GRAPHICS::DefaultResource::Texture2DBlackOpaque), *this);
+
+		UINT destCount = 1;
+		UINT srcCount[] = { 1 };
+		D3D12_CPU_DESCRIPTOR_HANDLE initializeTextures[1]
+		{
+			mHeightMap->GetTextureData()->GetSRV()
+		};
+		D_GRAPHICS_DEVICE::GetDevice()->CopyDescriptors(1, &mTexturesHeap, &destCount, destCount, initializeTextures, srcCount, D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+
+		// Updating parameter constants
+		auto& currentParamBuff = mParametersConstantsCPU[D_GRAPHICS_DEVICE::GetCurrentFrameResourceIndex()];
+		auto paramCB = reinterpret_cast<TerrainParametersConstants*>(currentParamBuff.Map());
+		paramCB->HeightFactor = mHeightFactor;
+		currentParamBuff.Unmap();
+
+		// Uploading
+		auto& context = D_GRAPHICS::CommandContext::Begin(L"Terrain Params Uploadier");
+		context.TransitionResource(mParametersConstantsGPU, D3D12_RESOURCE_STATE_COPY_DEST, true);
+		context.CopyBufferRegion(mParametersConstantsGPU, 0, currentParamBuff, 0, currentParamBuff.GetBufferSize());
+		context.TransitionResource(mParametersConstantsGPU, D3D12_RESOURCE_STATE_GENERIC_READ);
+		context.Finish(true);
+
+		return true;
+	}
+
+#ifdef _D_EDITOR
+	bool TerrainResource::DrawDetails(float params[])
+	{
+		bool valueChanged = false;
+
+		D_H_DETAILS_DRAW_BEGIN_TABLE();
+
+
+		{
+			D_H_DETAILS_DRAW_PROPERTY("Height Map");
+			D_H_RESOURCE_SELECTION_DRAW(TextureResource, mHeightMap, "Select Height Map", SetHeightMap);
+		}
+
+		{
+			D_H_DETAILS_DRAW_PROPERTY("Heght Factor");
+
+			float heightFactor = GetHeightFactor();
+			if (ImGui::DragFloat("##HeightFactor", &heightFactor))
+			{
+				SetHeightFactor(heightFactor);
+			}
+		}
+
+		D_H_DETAILS_DRAW_END_TABLE();
+
+		if (valueChanged)
+		{
+			MakeDiskDirty();
+			MakeGpuDirty();
+		}
+
+		return valueChanged;
+	}
+#endif
+
+	void TerrainResource::SetHeightMap(D_RESOURCE::ResourceHandle handle)
+	{
+		mHeightMap = D_RESOURCE::GetResource<TextureResource>(handle, *this);
+		MakeDiskDirty();
+		MakeGpuDirty();
+	}
+
+	bool TerrainResource::IsDirtyGPU() const
+	{
+		return Resource::IsDirtyGPU() ||
+			(mHeightMap.IsValid() && mHeightMap->IsDirtyGPU());
+	}
+
+}
---
+++ src/Renderer/Resources/TerrainResource.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "Renderer/FrameResource.hpp"
+#include "Renderer/GraphicsUtils/Buffers/GpuBuffer.hpp"
+#include "Renderer/GraphicsUtils/Buffers/UploadBuffer.hpp"
+#include "Renderer/GraphicsUtils/Memory/DescriptorHeap.hpp"
+#include "TextureResource.hpp"
+
+#include <ResourceManager/Resource.hpp>
+#include <ResourceManager/ResourceRef.hpp>
+
+#include "TerrainResource.generated.hpp"
+
+#ifndef D_GRAPHICS
+#define D_GRAPHICS Darius::Graphics
+#endif // !D_GRAPHICS
+
+namespace Darius::Graphics
+{
+	class DClass(Serialize, Resource) TerrainResource : public D_RESOURCE::Resource
+	{
+		D_CH_RESOURCE_BODY(TerrainResource, "Terrain", ".terrain");
+
+	public:
+
+#ifdef _D_EDITOR
+		virtual bool					DrawDetails(float params[]) override;
+#endif // _D_EDITOR
+
+		void							SetHeightMap(D_RESOURCE::ResourceHandle handle);
+		virtual bool					IsDirtyGPU() const override;
+
+		INLINE void						SetHeightFactor(float value)
+		{
+			mHeightFactor = value;
+			MakeDiskDirty();
+			MakeGpuDirty();
+		}
+
+		INLINE D3D12_GPU_VIRTUAL_ADDRESS GetParamsConstantsAddress() const { return mParametersConstantsGPU
+		.GetGpuVirtualAddress(); }
+		INLINE D3D12_GPU_DESCRIPTOR_HANDLE	GetTexturesHandle() const { return mTexturesHeap; }
+
+		INLINE operator D_CORE::CountedOwner() {
+			return D_CORE::CountedOwner{ GetName(), rttr::type::get<TerrainResource>(), this, 0, [&]() { MakeGpuDirty(); } };
+		}
+
+
+	protected:
+
+		virtual void					WriteResourceToFile(D_SERIALIZATION::Json& j) const override;
+		virtual void					ReadResourceFromFile(D_SERIALIZATION::Json const& j) override;
+		virtual bool					UploadToGpu() override;
+		virtual INLINE void				Unload() override { EvictFromGpu(); }
+
+	private:
+
+		TerrainResource(D_CORE::Uuid uuid, std::wstring const& path, std::wstring const& name, D_RESOURCE::DResourceId id, bool isDefault = false) :
+			Resource(uuid, path, name, id, isDefault),
+			mHeightFactor(300.f),
+			mHeightMap(GetAsCountedOwner()) {}
+
+		
+		DField(Get[const, &, inline])
+		D_RESOURCE::ResourceRef<TextureResource>		mHeightMap;
+
+		DField(Get[const, &, inline])
+		float											mHeightFactor;
+
+		D_GRAPHICS_BUFFERS::UploadBuffer				mParametersConstantsCPU[D_RENDERER_FRAME_RESOURCE::gNumFrameResources];
+		D_GRAPHICS_BUFFERS::ByteAddressBuffer			mParametersConstantsGPU;
+
+		D_GRAPHICS_MEMORY::DescriptorHandle				mTexturesHeap;
+
+	public:
+		Darius_Graphics_TerrainResource_GENERATED
+	};
+}
+
+File_TerrainResource_GENERATED
