943d5f74c89a970284a08e5058ccd5d8f4b4de83
Mohammad MDSA <samimd.77@gmail.com>
2023-11-30 19:21:36 +0330

    Remove boost assert

--- src/Utils/Assert.hpp
+++ src/Utils/Assert.hpp
@@ -1,11 +1,7 @@
 #include <cassert>
 #include <stdexcept>
-#include <boost/assert.hpp>
-#include <boost/static_assert.hpp>
 
-#define D_ASSERT(cond)			BOOST_ASSERT(cond)
-#define D_ASSERT_M(cond, msg)	BOOST_ASSERT_MSG(cond, msg)
-#define D_VERIFY(cond)			BOOST_VERIFY(cond)
-#define D_VERIFY_M(cond, msg)	BOOST_VERIFY_MSG(cond, msg)
-#define D_STATIC_ASSERT(cond)	BOOST_STATIC_ASSERT(cond)
-#define D_STATIC_ASSERT_M(cond, msg) BOOST_STATIC_ASSERT_MSG(cond, msg)
+#define D_ASSERT(expr)			assert(expr)
+#define D_ASSERT_M(expr, msg)	assert((expr)&&(msg))
+#define D_STATIC_ASSERT(...)	static_assert(__VA_ARGS__, #__VA_ARGS__)
+#define D_STATIC_ASSERT_M(...)	static_assert(__VA_ARGS__)
