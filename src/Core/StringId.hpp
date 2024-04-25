6ea2cdb750dda86006ad127eca3b04fac50085ac
Mohammad MDSA <samimd.77@gmail.com>
2024-04-25 23:13:28 +0330

    Add string id literal

--- src/Core/StringId.hpp
+++ src/Core/StringId.hpp
@@ -1,7 +1,7 @@
 #include "pch.hpp"
 
-#include "StringId/string_id.hpp"
-#include "StringId/database.hpp"
+#include <Libs/StringId/string_id.hpp>
+#include "Libs/StringId/database.hpp"
 
 
 #ifndef D_CORE
@@ -12,4 +12,10 @@ namespace Darius::Core
 {
 	using StringId = foonathan::string_id::string_id;
 	using StringIdDatabase = foonathan::string_id::default_database;
+
+}
+
+constexpr __forceinline foonathan::string_id::hash_type operator ""_Id(char const* str, std::size_t)
+{
+	return foonathan::string_id::detail::sid_hash(str);
 }
