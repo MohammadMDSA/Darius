e6f2308e9e122a00b099f68d1c80ea471ed443be
Mohammad MDSA <samimd.77@gmail.com>
2024-05-08 10:52:40 +0330

    Add audio device selection and preferred device

--- src/Audio/AudioManager.cpp
+++ src/Audio/AudioManager.cpp
@@ -10,12 +10,20 @@
 
 #include <Audio.h>
 
+#if _D_EDITOR
+#include <imgui.h>
+#endif // _EDITOR
+
+
 bool											_initialize = false;
 std::unique_ptr<DirectX::AudioEngine>			AudioEngineInst;
 std::atomic_bool								ShouldReset = false;
 D_CORE::SignalConnection						AppSuspendSignalConnection;
 D_CORE::SignalConnection						AppResumeSignalConnection;
 D_CORE::SignalConnection						NewDeviceSignalConnection;
+
+// Options
+std::string										PreferredDeviceId = "";
 
 using namespace DirectX;
 
@@ -25,11 +33,35 @@ namespace Darius::Audio
 	void ResumeAudioEngine();
 	void NewAudioDeviceConnected();
 
-	void Initialize(D_SERIALIZATION::Json const&)
+	bool IsDevicePresent(std::wstring const& deviceId)
+	{
+		auto enumList = AudioEngine::GetRendererDetails();
+		if(!enumList.empty())
+		{
+			for(const auto& it : enumList)
+			{
+				if(std::wstring(it.deviceId) == deviceId)
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	std::wstring GetAvailablePreferredDeviceIfAvailable()
+	{
+		std::wstring pref = STR2WSTR(PreferredDeviceId);
+		return IsDevicePresent(pref) ? pref : std::wstring();
+	}
+
+	void Initialize(D_SERIALIZATION::Json const& settings)
 	{
 		D_ASSERT(!_initialize);
 		_initialize = true;
 		ShouldReset = false;
+
+		D_H_OPTIONS_LOAD_BASIC_DEFAULT("Audio.PreferredDeviceId", PreferredDeviceId, "");
 
 		// Setting up audio engine
 		AUDIO_ENGINE_FLAGS eflags = AudioEngine_Default;
@@ -37,28 +69,10 @@ namespace Darius::Audio
 		eflags |= AudioEngine_Debug;
 #endif
 
-		auto enumList = AudioEngine::GetRendererDetails();
+		AudioEngineInst = std::make_unique<DirectX::AudioEngine>(eflags, nullptr, GetAvailablePreferredDeviceIfAvailable().c_str());
 
-		DirectX::AudioEngine::RendererDetail det;
-		if(enumList.empty())
-		{
-			// No audio devices
-		}
-		else
-		{
-			for(const auto& it : enumList)
-			{
-				det = it;
-				int i = 1;
-				if(i)
-					continue;
-			}
-		}
-
-		AudioEngineInst = std::make_unique<DirectX::AudioEngine>(eflags, nullptr, det.deviceId.c_str());
-
-		AppSuspendSignalConnection = D_APP::SubscribeOnAppSuspended(SuspendAudioEngine);
-		AppResumeSignalConnection = D_APP::SubscribeOnAppResuming(ResumeAudioEngine);
+		AppSuspendSignalConnection = D_APP::SubscribeOnAppDeactivated(SuspendAudioEngine);
+		AppResumeSignalConnection = D_APP::SubscribeOnAppActivated(ResumeAudioEngine);
 		NewDeviceSignalConnection = D_APP::SubscribeOnNewAudioDeviceConnected(NewAudioDeviceConnected);
 
 		AudioResource::Register();
@@ -80,7 +94,31 @@ namespace Darius::Audio
 #ifdef _D_EDITOR
 	bool OptionsDrawer(D_SERIALIZATION::Json& options)
 	{
-		return false;
+		D_H_OPTION_DRAW_BEGIN();
+
+		{
+			D_CONTAINERS::DVector<char const*> deviceTextsC;
+			D_CONTAINERS::DVector<std::string> deviceTexts;
+			D_CONTAINERS::DVector<std::string> deviceIds;
+			std::string selectedText = "";
+			for(auto const& it : AudioEngine::GetRendererDetails())
+			{
+				std::string deviceText = WSTR2STR(it.description);
+				deviceTexts.push_back(deviceText);
+				deviceTextsC.push_back(deviceTexts.back().c_str());
+				std::string deviceId = WSTR2STR(it.deviceId);
+				deviceIds.push_back(deviceId);
+				if(PreferredDeviceId == deviceId)
+					selectedText = deviceText;
+			}
+			bool alreadyChanged = settingsChanged;
+			D_H_OPTION_DRAW_CHOICE_VAL("Preferred Device", "Audio.PreferredDeviceId", PreferredDeviceId, deviceTextsC, deviceIds, selectedText.c_str(), deviceIds.size());
+			if(!alreadyChanged && settingsChanged)
+				ShouldReset.store(true);
+		}
+
+		D_H_OPTION_DRAW_END();
+
 	}
 #endif
 
@@ -98,7 +136,7 @@ namespace Darius::Audio
 		if(ShouldReset.load())
 		{
 			ShouldReset.store(false);
-			if(!AudioEngineInst->Reset())
+			if(!AudioEngineInst->Reset(nullptr, GetAvailablePreferredDeviceIfAvailable().c_str()))
 			{
 				D_LOG_ERROR("Could not reset the audio engine due to unavailability of any audio devices.");
 			}
