36fd41dd58611edcfac8abe26dc388d2b8125abb
Mohammad MDSA <samimd.77@gmail.com>
2024-05-20 09:17:28 +0330

    Add spatial proxy scene

---
+++ src/Scene/Proxy/SceneProxy.hpp
@@ -0,0 +1,99 @@
+#pragma once
+
+#include "Scene/Scene.hpp"
+#include "Scene/GameObject.hpp"
+
+#include <Core/Containers/Map.hpp>
+#include <Core/Memory/Allocators/PagedAllocator.hpp>
+#include <Utils/Common.hpp>
+
+#include "SceneProxy.generated.hpp"
+
+#ifndef D_SCENE
+#define D_SCENE Darius::Scene
+#endif // !D_SCENE
+
+namespace Darius::Scene
+{
+	template <typename SceneObject>
+	class SceneProxy
+	{
+	public:
+		SceneProxy(SceneManager* scene)
+		{
+
+		}
+
+		virtual ~SceneProxy()
+		{
+			for(auto& [k, v] : mGameObjectLookup)
+				mSceneAllocator.Free(v);
+			mSceneAllocator.Reset();
+		}
+
+		INLINE SceneObject*					LookupGameObject(GameObject* go) const
+		{
+			auto search = mGameObjectLookup.find(go);
+			if(search == mGameObjectLookup.end())
+				return nullptr;
+
+			return search->second;
+		}
+
+	protected:
+		SceneObject* AddActor(GameObject* go)
+		{
+			SceneObject* goProxy = mSceneAllocator.Alloc(std::move(go), std::move(this));
+			mGameObjectLookup[go] = goProxy;
+			return goProxy;
+		}
+
+		virtual bool						RemoveActor(GameObject* go)
+		{
+			auto search = mGameObjectLookup.find(go);
+			if(search == mGameObjectLookup.end())
+				return false;
+
+			return RemoveActor(search->second);
+		}
+
+		INLINE bool							RemoveActor(SceneObject* proxy)
+		{
+			D_ASSERT(proxy);
+
+			GameObject* go = proxy->GetGameObject();
+			if(!mGameObjectLookup.contains(go))
+				return false;
+			mGameObjectLookup.erase(go);
+			mSceneAllocator.Free(proxy);
+			return true;
+		}
+
+	private:
+		SceneManager*						mScene;
+		D_CONTAINERS::DUnorderedMap<GameObject*, SceneObject*> mGameObjectLookup;
+		D_MEMORY::PagedAllocator<SceneObject> mSceneAllocator;
+	};
+
+	template <typename ObjectType>
+	class SceneProxyObjectBase
+	{
+	public:
+
+		SceneProxyObjectBase(GameObject* go, SceneProxy<ObjectType>* scene) :
+			mGameObject(go),
+			mSceneProxy(scene)
+		{ }
+
+		virtual ~SceneProxyObjectBase() { }
+
+		INLINE GameObject*					GetGameObject() const { return mGameObject; }
+		INLINE SceneProxy<ObjectType>*		GetScene() const { return mSceneProxy; }
+
+	private:
+		GameObject*							mGameObject;
+		SceneProxy<ObjectType>*				mSceneProxy;
+	};
+}
+
+File_SceneProxy_GENERATED
---
+++ src/Scene/Proxy/SpacialSceneProxy.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "SceneProxy.hpp"
+
+#include <Core/Signal.hpp>
+#include <Math/Bounds/BoundingBox.hpp>
+#include <Math/Bounds/DynamicBVH.hpp>
+
+#include "SpacialSceneProxy.generated.hpp"
+
+#ifndef D_SCENE
+#define D_SCENE Darius::Scene
+#endif // !D_SCENE
+
+namespace Darius::Math
+{
+	struct Transform;
+}
+
+namespace Darius::Scene
+{
+	template <typename Object>
+	class SpacialSceneProxy : public SceneProxy<Object>
+	{
+	public:
+		SpacialSceneProxy(SceneManager* scene) :
+			SceneProxy<Object>(scene)
+		{ }
+
+		virtual void						Update(float /*dt*/)
+		{
+			mBVH.OptimizeIncremental(1);
+		}
+
+		INLINE D_MATH_BOUNDS::DynamicBVH<Object*>& GetBVH() { return mBVH; }
+		INLINE D_MATH_BOUNDS::DynamicBVH<Object*> const& GetBVH() const { return mBVH; }
+
+	private:
+		D_MATH_BOUNDS::DynamicBVH<Object*> mBVH;
+	};
+
+	template <typename Object>
+	class SpacialSceneProxyObject : public SceneProxyObjectBase<Object>
+	{
+	public:
+		SpacialSceneProxyObject(GameObject* go, SceneProxy<Object>* scene) :
+			SceneProxyObjectBase<Object>(go, scene)
+		{
+			D_ASSERT(dynamic_cast<SpacialSceneProxy<Object>*>(scene));
+			mTransformChangeSignal = go->GetTransform()->mWorldChanged.ConnectGenericObject(this, &SpacialSceneProxyObject::OnWorldTransformChanged);
+
+			// Creating bvh node
+			mBvhNodeId = GetSpacialScene()->GetBVH().Insert(GetAabb(), reinterpret_cast<Object*>(this));
+		}
+
+		virtual ~SpacialSceneProxyObject()
+		{
+			GetSpacialScene()->GetBVH().Remove(mBvhNodeId);
+			mTransformChangeSignal.disconnect();
+		}
+
+		virtual void						OnWorldTransformChanged(D_MATH::TransformComponent* target, D_MATH::Transform const& newTrans)
+		{
+			GetSpacialScene()->GetBVH().Update(mBvhNodeId, GetAabb());
+		}
+
+		virtual D_MATH_BOUNDS::Aabb			GetAabb() const { return D_MATH_BOUNDS::Aabb(); };
+
+		void								UpdateBvhNode()
+		{
+			GetSpacialScene()->GetBVH().Update(mBvhNodeId, GetAabb());
+		}
+
+		INLINE SpacialSceneProxy<Object>*	GetSpacialScene() const { return static_cast<SpacialSceneProxy<Object>*>(this->GetScene()); }
+
+	private:
+		
+		D_CORE::SignalConnection			mTransformChangeSignal;
+		D_MATH_BOUNDS::DynamicBVH<Object*>::ID mBvhNodeId;
+	};
+}
