449e60ce9526b1e4cb98f09cd8a016d53b4b2ec6
SamiMD <samimd.77@gmail.com>
2024-09-30 16:41:34 -0400

    Add sampler desc comparator and equality and serialization

--- src/Graphics/GraphicsUtils/SamplerManager.cpp
+++ src/Graphics/GraphicsUtils/SamplerManager.cpp
@@ -5,11 +5,14 @@
 #include "Graphics/GraphicsDeviceManager.hpp"
 
 #include <Core/Hash.hpp>
+#include <Core/Serialization/Json.hpp>
+#include <Core/Serialization/TypeSerializer.hpp>
 #include <Utils/Assert.hpp>
 
 #include <map>
 
 using namespace std;
+using namespace D_SERIALIZATION;
 
 namespace
 {
@@ -45,4 +48,39 @@ namespace Darius::Graphics::Utils
 		D_GRAPHICS_DEVICE::GetDevice()->CreateSampler(this, Handle);
 	}
 
+	void SamplerDesc::Serialize(Json& json) const
+	{
+		D_MATH::Color border(BorderColor[0], BorderColor[1], BorderColor[2], BorderColor[3]);
+
+		json.clear();
+		json["Filter"] = (uint32_t)Filter;
+		json["AddressU"] = (uint32_t)AddressU;
+		json["AddressV"] = (uint32_t)AddressV;
+		json["AddressW"] = (uint32_t)AddressW;
+		json["MipLODBias"] = MipLODBias;
+		json["MaxAnisotropy"] = (uint32_t)MaxAnisotropy;
+		json["ComparisonFunc"] = (uint32_t)ComparisonFunc;
+		D_SERIALIZATION::Serialize(border, json["BorderColor"]);
+		json["MinLOD"] = (float)MinLOD;
+		json["MaxLOD"] = (float)MaxLOD;
+	}
+
+	void SamplerDesc::Deserialize(Json const& json)
+	{
+		D_MATH::Color border;
+		Filter = (D3D12_FILTER)json["Filter"].get<uint32_t>();
+		AddressU = (D3D12_TEXTURE_ADDRESS_MODE)json["AddressU"].get<uint32_t>();
+		AddressV = (D3D12_TEXTURE_ADDRESS_MODE)json["AddressV"].get<uint32_t>();
+		AddressW = (D3D12_TEXTURE_ADDRESS_MODE)json["AddressW"].get<uint32_t>();
+		MipLODBias = json["MipLODBias"];
+		MaxAnisotropy = json["MaxAnisotropy"].get<uint32_t>();
+		ComparisonFunc = (D3D12_COMPARISON_FUNC)json["ComparisonFunc"].get<uint32_t>();
+		D_SERIALIZATION::Deserialize(border, json["BorderColor"]);
+		BorderColor[0] = border.GetR();
+		BorderColor[1] = border.GetG();
+		BorderColor[2] = border.GetB();
+		BorderColor[3] = border.GetA();
+		MinLOD = json["MinLOD"];
+		MaxLOD = json["MaxLOD"];
+	}
 }
--- src/Graphics/GraphicsUtils/SamplerManager.hpp
+++ src/Graphics/GraphicsUtils/SamplerManager.hpp
@@ -1,18 +1,6 @@
-//
-// Copyright (c) Microsoft. All rights reserved.
-// This code is licensed under the MIT License (MIT).
-// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
-// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
-// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
-// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
-//
-// Developed by Minigraph
-//
-// Author:  James Stanard
-//
-
 #pragma once
 
+#include <Core/Serialization/Json.hpp>
 #include <Math/Color.hpp>
 
 #ifndef D_GRAPHICS_UTILS
@@ -21,48 +9,69 @@
 
 namespace Darius::Graphics::Utils
 {
-    class SamplerDesc : public D3D12_SAMPLER_DESC
-    {
-    public:
-        // These defaults match the default values for HLSL-defined root
-        // signature static samplers.  So not overriding them here means
-        // you can safely not define them in HLSL.
-        SamplerDesc()
-        {
-            Filter = D3D12_FILTER_ANISOTROPIC;
-            AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
-            AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
-            AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
-            MipLODBias = 0.0f;
-            MaxAnisotropy = 16;
-            ComparisonFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
-            BorderColor[0] = 1.0f;
-            BorderColor[1] = 1.0f;
-            BorderColor[2] = 1.0f;
-            BorderColor[3] = 1.0f;
-            MinLOD = 0.0f;
-            MaxLOD = D3D12_FLOAT32_MAX;
-        }
+	class SamplerDesc : public D3D12_SAMPLER_DESC
+	{
+	public:
+		// These defaults match the default values for HLSL-defined root
+		// signature static samplers.  So not overriding them here means
+		// you can safely not define them in HLSL.
+		SamplerDesc()
+		{
+			Filter = D3D12_FILTER_ANISOTROPIC;
+			AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+			AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+			AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+			MipLODBias = 0.0f;
+			MaxAnisotropy = 16;
+			ComparisonFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
+			BorderColor[0] = 1.0f;
+			BorderColor[1] = 1.0f;
+			BorderColor[2] = 1.0f;
+			BorderColor[3] = 1.0f;
+			MinLOD = 0.0f;
+			MaxLOD = D3D12_FLOAT32_MAX;
+		}
 
-        void SetTextureAddressMode(D3D12_TEXTURE_ADDRESS_MODE AddressMode)
-        {
-            AddressU = AddressMode;
-            AddressV = AddressMode;
-            AddressW = AddressMode;
-        }
+		void SetTextureAddressMode(D3D12_TEXTURE_ADDRESS_MODE AddressMode)
+		{
+			AddressU = AddressMode;
+			AddressV = AddressMode;
+			AddressW = AddressMode;
+		}
 
-        void SetBorderColor(D_MATH::Color Border)
-        {
-            BorderColor[0] = Border.GetR();
-            BorderColor[1] = Border.GetG();
-            BorderColor[2] = Border.GetB();
-            BorderColor[3] = Border.GetA();
-        }
+		void SetBorderColor(D_MATH::Color Border)
+		{
+			BorderColor[0] = Border.GetR();
+			BorderColor[1] = Border.GetG();
+			BorderColor[2] = Border.GetB();
+			BorderColor[3] = Border.GetA();
+		}
 
-        // Allocate new descriptor as needed; return handle to existing descriptor when possible
-        D3D12_CPU_DESCRIPTOR_HANDLE CreateDescriptor(void);
+		bool operator ==(SamplerDesc const& other) const
+		{
+			return
+				this->Filter == other.Filter &&
+				this->AddressU == other.AddressU &&
+				this->AddressV == other.AddressV &&
+				this->AddressW == other.AddressW &&
+				this->MipLODBias == other.MipLODBias &&
+				this->MaxAnisotropy == other.MaxAnisotropy &&
+				this->ComparisonFunc == other.ComparisonFunc &&
+				this->BorderColor[0] == other.BorderColor[0] &&
+				this->BorderColor[1] == other.BorderColor[1] &&
+				this->BorderColor[2] == other.BorderColor[2] &&
+				this->BorderColor[3] == other.BorderColor[3] &&
+				this->MinLOD == other.MinLOD &&
+				this->MaxLOD == other.MaxLOD;
+		}
 
-        // Create descriptor in place (no deduplication).  Handle must be preallocated
-        void CreateDescriptor(D3D12_CPU_DESCRIPTOR_HANDLE Handle);
-    };
+		// Allocate new descriptor as needed; return handle to existing descriptor when possible
+		D3D12_CPU_DESCRIPTOR_HANDLE CreateDescriptor(void);
+
+		// Create descriptor in place (no deduplication).  Handle must be preallocated
+		void CreateDescriptor(D3D12_CPU_DESCRIPTOR_HANDLE Handle);
+
+		void Serialize(Darius::Core::Serialization::Json& json) const;
+		void Deserialize(Darius::Core::Serialization::Json const& json);
+	};
 }
